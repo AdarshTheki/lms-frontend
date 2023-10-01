b9291c74975cef916fd2c76da0be79423f809a4e
Adarsh Verma <adarshverma549@gmail.com>
2023-10-01 23:34:41 +0530

    store with middleware config file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './authSlice';
+import courseSlice from './courseSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    course: courseSlice,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
   devTools: true,
 });
